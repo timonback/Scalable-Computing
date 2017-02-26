920c04f8bfe4185cb7a94467bba603c7dc5f9f84
Timo Smit <t.smitje@hotmail.com>
2017-02-27 00:14:23 +0100

    Fixed bug in Scala importer

--- importer/src/main/scala/Fetcher.scala
+++ importer/src/main/scala/Fetcher.scala
@@ -1,5 +1,6 @@
 import com.mongodb.spark.MongoSpark
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scalaj.http.{Http, HttpResponse}
 
@@ -16,8 +17,11 @@ object Fetcher {
     // val response: HttpResponse[String] = Http("https://api.nytimes.com/svc/archive/v1/2016/1.json").param("api-key", "6abaec279f9d4c4dad5459690c7b4563").asString
     val response: HttpResponse[String] = Http("https://newsapi.org/v1/articles").param("source", "techcrunch").param("apikey", "919447fbe85f4ad09e7ee0b868efdbd4").asString
 
-    val df = spark.read.json(response.body)
-    val articles = df.select(org.apache.spark.sql.functions.explode(df.col("articles")).as("a"))
+    val rdd: RDD[String] = spark.sparkContext.makeRDD(response.body :: Nil)
+
+    val df: DataFrame = spark.read.json(rdd)
+
+    val articles: DataFrame = df.select(org.apache.spark.sql.functions.explode(df.col("articles")).as("a"))
 
     // df.printSchema()
 
