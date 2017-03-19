9517801adcaf8b4a7f9c561d781307e4526518e8
Timon Back <T.Back@student.rug.nl>
2017-03-19 22:10:59 +0100

    Added latest tag to docker images

--- swarm.sh
+++ swarm.sh
@@ -22,8 +22,8 @@ sudo chown $USER /vol
 sudo chgrp $USER /vol
 mkdir -p /vol/mongo /vol/mongo/data /vol/mongo/config
 
-#Add database
-docker service create --replicas 2 --network services --endpoint-mode dnsrr --mount type=bind,source=/vol/mongo/data,target=/data/db --mount type=bind,source=/vol/mongo/config,target=/data/configdb --name mongo mongo mongod --replSet mongoReplica
+#Add database --endpoint-mode dnsrr
+docker service create --name mongo -p 27017:27017 --replicas 2 --network services --mount type=bind,source=/vol/mongo/data,target=/data/db --mount type=bind,source=/vol/mongo/config,target=/data/configdb mongo mongod --replSet mongoReplica
 
 ##on first mongo node
 # docker exec -it mongo.*container-name* bash -c 'IP="$(hostname -i)"; echo "MasterIP: $IP"; mongo --eval "rs.initiate({ _id: \"mongoReplica\", members: [{ _id: 0, host: \"'$IP':27017\" }], settings: { getLastErrorDefaults: { w: \"majority\" }}})"'
@@ -34,7 +34,7 @@ docker service create --replicas 2 --network services --endpoint-mode dnsrr --mo
 #docker exec -it *container-name* hostname -i
 
 #Add visualization service
-docker service create --name visualization -p 9000:9000 -e MONGO_ADDRESS=mongo --replicas 1 --network services timonback/newsforyou-visualization
+docker service create --name visualization -p 9000:9000 -e MONGO_ADDRESS=mongo --replicas 1 --network services timonback/newsforyou-visualization:latest
 
 #Add spark master
 docker service create --name spark-master --hostname spark-master -p 7077:7077 -p 8080:8080--replicas 1 --network services singularities/spark start-spark master
@@ -46,9 +46,6 @@ docker service create --name spark-worker --hostname spark-worker --replicas 2 -
 #docker service create --name spark-task-submitter --replicas 1 --network services --restart-delay 1m singularities/spark spark-submit --class org.apache.spark.examples.SparkPi --master spark://spark-master:7077 /usr/local/spark-2.1.0/examples/jars/spark-examples_2.11-2.1.0.jar 10
 
 #Add spark task submitter (1min waiting inbetween runs) - recommendation
-docker service create --name spark-recommender-submitter -e MONGO_ADDRESS=mongo -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator.newsforyou-recommendator-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar -e USE_DUMMY_DATA=true --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator
-
-
-
+docker service create --name spark-recommender-submitter -e MONGO_ADDRESS=mongo -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator.newsforyou-recommendator-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar -e USE_DUMMY_DATA=true --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator:latest
 
 
