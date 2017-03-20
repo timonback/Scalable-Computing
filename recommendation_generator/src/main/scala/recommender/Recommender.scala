76c306697ef295fa854d5b2cef54d1d9f3e9c4fa
remi brandt <r.brandt@student.rug.nl>
2017-03-20 13:17:28 +0100

    Already rated articles are not recommended anymore

--- recommendation_generator/src/main/scala/recommender/Recommender.scala
+++ recommendation_generator/src/main/scala/recommender/Recommender.scala
@@ -29,7 +29,7 @@ object Recommender extends App{
 
     var sparkUrl = "spark://"+sparkAddress+":"+sparkPort
 
-    // Temporary:
+    // TEMP
     //sparkUrl = "local"
 
     val mongoUrl = "mongodb://"+dbAddress+":"+dbPort+"/"+dbKeySpace
@@ -47,14 +47,16 @@ object Recommender extends App{
       .getOrCreate()
     sc = ss.sparkContext
 
+
     var jarFileEnv = sys.env.get("SPARK_JAR").getOrElse("")
     println("Add jar file(s) to spark: " + jarFileEnv)
     for(jarFile <- jarFileEnv.split(",")) {
       sc.addJar(jarFile)
     }
+   
 
     var ratingsRDD : RDD[Rating] =  null
-    if(useDummyDataOpt.isEmpty) {
+    if(useDummyDataOpt.isEmpty) { 
       // Or load from db
       println("Loading rating data from DB")
       var temp = MongoSpark.load(sc).toDF.rdd
@@ -66,27 +68,31 @@ object Recommender extends App{
       for( user <- 0 to 200-1){
         for( article <- 0 to 100-1){
           val r = scala.util.Random
-          if(r.nextInt(100) >= 80) {
-            ratings +:= Rating(user,article,1.0)
+          if(r.nextInt(100) >= 50) {
+            ratings +:= Rating(user,article,r.nextInt(1000)*.1)
           }
         }
       }
       ratingsRDD = sc.parallelize(ratings)
     }
 
-
     // Learn Model
-    val model : ALSModel = learnModel(ratingsRDD,2,10,200,100,1.5)
+    val numIterations = 4
+    val numLatentFactors = 5
+    val articleIndices = ratingsRDD.groupBy(_.article).map(a=>a._1).collect()
+    val regularization = 0.01
+    val numPredictions = 10
+    val model : ALSModel = learnModel(ratingsRDD,numIterations,numLatentFactors,articleIndices,regularization)
 
     // Generate recommendations
-    val recommendations = recommendArticles(10, model)
+    val recommendations = recommendArticles(numPredictions, model,ratingsRDD)
 
     // Store recommendations
     storeRecommendations(ss,recommendations)
 
     // Generate recommendations for anonymous user
     var newRatingsRDD = ratingsRDD
-    var newRecommendations = recommendArticlesForNewUsers(newRatingsRDD, 10 , 10, 1.5, model, 10)
+    var newRecommendations = recommendArticlesForNewUsers(newRatingsRDD, numIterations , numLatentFactors, regularization, model, numPredictions)
 
     // Store new recommendations
     storeRecommendations(ss,newRecommendations)
@@ -94,10 +100,10 @@ object Recommender extends App{
     sc.stop()
   }
 
-  def learnModel(ratings: RDD[Rating], numIterations: Int, numLatentFactors : Int,numUsers : Int,numArticles : Int, regularization: Double): ALSModel = {
+  def learnModel(ratings: RDD[Rating], numIterations: Int, numLatentFactors : Int,articleIndices : Array[Int], regularization: Double): ALSModel = {
     // Initialize User and Article Factors
     var userFactors: RDD[(Int, Array[Double])] = null
-    var articleFactors: RDD[(Int, Array[Double])] = initialize(numArticles, numLatentFactors)
+    var articleFactors: RDD[(Int, Array[Double])] = initialize(articleIndices, numLatentFactors)
 
     // Learn Model
     for (i <- 0 until numIterations) {
@@ -202,31 +208,33 @@ object Recommender extends App{
     entries
   }
 
-
   def inverse(mat: DenseMatrix): DenseMatrix = {
     var dm  = new breeze.linalg.DenseMatrix[Double](mat.numRows,mat.numCols,mat.values)
     dm = inv(dm)
     new DenseMatrix(dm.rows,dm.cols,dm.data)
   }
 
-  def initialize( number : Int, numLatentFactors : Int) : RDD[(Int, Array[Double])] = {
+  def initialize( indexes : Array[Int], numLatentFactors : Int) : RDD[(Int, Array[Double])] = {
     var result: Array[(Int, Array[Double])] = Array()
-    for (x <- 0 until number) {
+    indexes.foreach( x => {
       var array : Array[Double] = Array()
       for (y <- 0 until numLatentFactors) {
         val r = scala.util.Random
-        array +:= r.nextInt(100)*.1
+        array +:= r.nextInt(1000)*.1
       }
       result +:= (x,array)
-    }
+    })
     sc.parallelize(result)
   }
 
-  def recommendArticles(number: Int, model: ALSModel) : RDD[(Int,Array[Int])] = {
-    var a = vectorsToBlockMatrix(model.userFactors).transpose
-    var b = vectorsToBlockMatrix(model.articleFactors)
+  def recommendArticles(number: Int, model: ALSModel,ratings : RDD[Rating]) : RDD[(Int,Array[Int])] = {
+    var a = vectorsToBlockMatrix(model.userFactors)
+    var b = vectorsToBlockMatrix(model.articleFactors).transpose
 
     var c = a.multiply(b)
+    // Remove already rated
+    val ratingsMatrix : BlockMatrix = new CoordinateMatrix(ratings.map(a=> MatrixEntry(a.user,a.article,Double.PositiveInfinity)),c.numRows(),c.numCols()).toBlockMatrix()
+    c.subtract(ratingsMatrix)
 
     var lengths : RDD[Array[Double]] = c.toIndexedRowMatrix().rows.map(a=> a.vector.toArray)
 
@@ -244,13 +252,13 @@ object Recommender extends App{
     }
     var newModel = ALSModel(userFactors, model.articleFactors)
     // Recommend Articles
-    recommendArticles(number, newModel)
+    recommendArticles(number, newModel,ratings)
   }
 
   def getLargestN(array: Array[(Double,Int)],number:Int) : Array[Int] = {
     var buffer : Array[(Double,Int)] = Array()
     (0 until number).foreach(a => buffer +:= (0.0,0))
-    array.foreach(a=>  buffer = buffer.map(b=> if(b._1 < a._1) {(a._1,a._2)}else{(b._1,b._2)}) )
+    array.foreach(a=> buffer = buffer.map(b=> if(b._1 < a._1) {(a._1,a._2)}else{(b._1,b._2)}) )
     buffer.map(a=>a._2)
   }
 
