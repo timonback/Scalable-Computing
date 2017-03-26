3227c754d1c6dc908993f29211f7b71bd80dde36
remi brandt <r.brandt@student.rug.nl>
2017-03-26 17:47:32 +0200

    Streaing part all done except for actual linkage with kafka

--- recommendation_generator/build.sbt
+++ recommendation_generator/build.sbt
@@ -19,7 +19,9 @@ libraryDependencies ++= Seq(
   "org.mongodb.spark" %% "mongo-spark-connector" % "2.0.0",
   "org.scalanlp" %% "breeze" % "0.13",
   "org.scalanlp" %% "breeze-natives" % "0.13",
-  "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.2",
-  "org.apache.kafka" % "kafka_2.10" % "0.8.2.2",
-  "org.apache.kafka" % "kafka-clients" % "0.8.2.2"
+  "org.apache.spark" % "spark-streaming_2.11" % "2.0.0-preview",
+  "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % "2.0.0-preview",
+  "org.apache.kafka" % "kafka_2.11" % "0.8.2.2",
+  "org.apache.kafka" % "kafka-clients" % "0.8.2.2",
+  "org.mongodb.spark" % "mongo-spark-connector_2.11" % "2.0.0"
 )
--- recommendation_generator/src/main/scala/recommender/StreamingRecommender.scala
+++ recommendation_generator/src/main/scala/recommender/StreamingRecommender.scala
@@ -3,40 +3,50 @@ package recommender
 import breeze.linalg._
 import breeze.optimize.linear.PowerMethod.BDM
 import com.mongodb.spark.MongoSpark
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.distributed.{BlockMatrix, CoordinateMatrix, MatrixEntry, _}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode, SparkSession}
 import org.apache.spark.mllib.linalg.{DenseMatrix, DenseVector, Matrices, Vector}
 import _root_.kafka.serializer.DefaultDecoder
 import _root_.kafka.serializer.StringDecoder
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming._
+import com.mongodb.spark._
+import com.mongodb.spark.config.ReadConfig
+import com.mongodb.spark.sql.fieldTypes.ObjectId
+import org.bson.Document
 
 object StreamingRecommender extends App {
   var sc: SparkContext = _
+  var ss: SparkSession = _
+
+  var sparkAddress : String = _
+  var sparkPort : String = _
+  var dbAddress : String = _
+  var dbPort : Long = _
+  var dbKeySpace : String = _
+  var useDummyDataOpt : Option[String] = _
+  var sparkUrl : String = _
+  var mongoUrl : String = _
 
   override
   def main(args: Array[String]) = {
-    val sparkAddress = sys.env.get("SPARK_ADDRESS").getOrElse("localhost")
-    val sparkPort = sys.env.get("SPARK_PORT").getOrElse("7077")
 
-    val dbAddress = sys.env.get("MONGO_ADDRESS").getOrElse("localhost")
-    val dbPort = sys.env.get("MONGO_PORT").getOrElse("27017").toLong
-    val dbKeySpace = sys.env.get("MONGO_KEYSPACE").getOrElse("newsForYou")
-
-    val useDummyDataOpt = sys.env.get("USE_DUMMY_DATA")
-
-    var sparkUrl = "spark://" + sparkAddress + ":" + sparkPort
-
-    val mongoUrl = "mongodb://" + dbAddress + ":" + dbPort + "/" + dbKeySpace
+    sparkAddress = sys.env.get("SPARK_ADDRESS").getOrElse("localhost")
+    sparkPort = sys.env.get("SPARK_PORT").getOrElse("7077")
+    dbAddress = sys.env.get("MONGO_ADDRESS").getOrElse("localhost")
+    dbPort = sys.env.get("MONGO_PORT").getOrElse("27017").toLong
+    dbKeySpace = sys.env.get("MONGO_KEYSPACE").getOrElse("newsForYou")
+    useDummyDataOpt = sys.env.get("USE_DUMMY_DATA")
+    sparkUrl = "spark://" + sparkAddress + ":" + sparkPort
+    mongoUrl = "mongodb://" + dbAddress + ":" + dbPort + "/" + dbKeySpace
 
     println("Spark expected at: " + sparkUrl)
     println("Mongo expected at: " + mongoUrl)
 
-
-    val ss = SparkSession
+    ss = SparkSession
       .builder()
       .master(sparkUrl)
       .appName("recommender")
@@ -44,18 +54,72 @@ object StreamingRecommender extends App {
       .config("spark.mongodb.output.uri", mongoUrl + ".recommendations")
       .getOrCreate()
     sc = ss.sparkContext
-    sc.setLogLevel("ERROR")
 
-    var model = loadModel()
+//    val Array(zkQuorum, group, topics, numThreads) = Array("localhost:2181","ratingsGroup","ratingsTopic","1")
+//    val ssc = new StreamingContext(sc, Seconds(3))
+//    ssc.checkpoint("checkpoint")
+//    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+//    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+//    val words = lines.flatMap(_.split(" ")).map(a=>processLine(a)).print()
+//
+//    ssc.start()
+//    ssc.awaitTermination()
 
-    // TODO: new rating -> remove from recommendations of user (make user key in db?)
-    // TODO: check length array, length == 0? then:
-
-    val ratings = loadRatings() // TODO: read from DB all ratings by user (make user key in db?)
-
-    if(true){ // TODO: check if updated
-      model = loadModel()
+    while(true){
+      processLine("286,91,0.25648")
+      Thread.sleep(1000)
     }
+
+    sc.stop()
+  }
+
+  def loadModel(): ALSModel = {
+
+    var inputCollection = mongoUrl + ".articleFactors"
+    var temp = MongoSpark.load(sc,ReadConfig(Map("spark.mongodb.input.uri" -> inputCollection))).toDF.rdd
+    var factors = temp.map(row => (row.getAs[Long]("articleId"),row.getAs[Array[Double]]("latentFactors")))
+
+    ALSModel(null,factors)
+  }
+
+  def processLine(a:String): String ={
+    val si = a.split(",")
+    val userId = si(0).toLong
+    val articleId = si(1).toLong
+    var inputCollection = mongoUrl + ".recommendations"
+    var temp = MongoSpark.load(sc,ReadConfig(Map("spark.mongodb.input.uri" -> inputCollection))).toDF
+    val rows = temp.filter(row => row.getAs[Long]("userid") == userId )
+
+    if(rows.count() == 1){
+      val row = rows.first()
+      val newArray : Array[Long] = row.getAs[Seq[Long]]("recommendations").filter(a=>a!=articleId).toArray
+
+      // Update recommendations in DB
+      val newDocs = Seq( (ObjectId(row.get(0).toString.substring(1, row.get(0).toString.length()-1)),userId, newArray) )
+
+      val df : DataFrame =  ss.createDataFrame( newDocs )
+      val lpDF = df.withColumnRenamed("_1", "_id").withColumnRenamed("_2", "userid").withColumnRenamed("_3", "recommendations")
+      lpDF.printSchema()
+      MongoSpark.write(lpDF).option("collection", "recommendations").mode(SaveMode.Append).save()
+
+      // Generate new recommendations
+      if(newArray.isEmpty) generateNewRatings(userId)
+    }
+    a
+  }
+
+  def loadRatings(userId : Long): RDD[Rating] ={
+    var inputCollection = mongoUrl + ".ratings"
+    var temp = MongoSpark.load(sc,ReadConfig(Map("spark.mongodb.input.uri" -> inputCollection))).toDF
+    val rows = temp.filter(row => row.getAs[Long]("userId") == userId )
+    rows.rdd.map(r=>Rating(r.getAs[Long]("userId"),r.getAs[Long]("articleId"),r.getAs[Double]("rating")))
+  }
+
+  def generateNewRatings(userId : Long): Unit ={
+    var model = loadModel()
+    val ratings = loadRatings(userId)
+    model = loadModel()
+
     val numIterations = 12
     val numLatentFactors = 35
     val numArticles = ratings.groupBy(_.article).map(a=>a._1).collect()
@@ -64,19 +128,6 @@ object StreamingRecommender extends App {
 
     val recommendations = recommendArticlesForNewUsers(ratings,numIterations,numLatentFactors,regularization,model,numPredictions)
     BatchRecommender.storeRecommendations(ss,recommendations,true)
-
-    sc.stop()
-  }
-
-  def loadModel(): ALSModel = {
-    var temp = MongoSpark.load(sc).toDF.rdd
-    val factors = temp.map(row => (row.getLong(0),row.getAs[Array[Double]](1) ))
-
-    ALSModel(null,factors)
-  }
-
-  def loadRatings(): RDD[Rating] ={
-    null
   }
 
   def recommendArticlesForNewUsers(ratings: RDD[Rating], numIterations: Int, numLatentFactors : Int, regularization: Double, model: ALSModel,number :Int): RDD[(Long, Array[Long])] = {
