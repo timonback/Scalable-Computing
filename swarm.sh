4192496138c9c9cf016ef6c61d8b3c0a1cdb0543
Timon Back <T.Back@student.rug.nl>
2017-03-29 00:15:00 +0200

    Updated swarm.sh

--- swarm.sh
+++ swarm.sh
@@ -49,7 +49,7 @@ docker service create --name visualization -p 9000:9000 -e MONGO_ADDRESS=mongo -
 docker service create --name importer -e MONGO_ADDRESS=mongo --replicas 1 --network services --restart-delay 24h timonback/newsforyou-importer:latest
 
 #Add rating generator
-docker service create --name streamer -e KAFKA_ADDRESS=kafka --replicas 1 --network services --restart-delay 1m timonback/newsforyou-streamer:latest
+docker service create --name streamer -e MONGO_ADDRESS=mongo -e KAFKA_ADDRESS=kafka --replicas 1 --network services timonback/newsforyou-streamer:latest
 
 
 #Add spark master
@@ -61,6 +61,9 @@ docker service create --name spark-worker --hostname spark-worker --replicas 2 -
 #Add spark task submitter (1min waiting inbetween runs) - example
 #docker service create --name spark-task-submitter --replicas 1 --network services --restart-delay 1m singularities/spark spark-submit --class org.apache.spark.examples.SparkPi --master spark://spark-master:7077 /usr/local/spark-2.1.0/examples/jars/spark-examples_2.11-2.1.0.jar 10
 
-#Add spark task submitter (1min waiting inbetween runs) - recommendation
-docker service create --name spark-recommender-submitter -e MONGO_ADDRESS=mongo -e KAFKA_ADDRESS=kafka -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator.newsforyou-recommendator-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar -e USE_DUMMY_DATA=true --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator-streamer:latest
+#Add spark task submitter - recommendation batch
+docker service create --name spark-submitter-recommender-batch -e MONGO_ADDRESS=mongo -e KAFKA_ADDRESS=kafka -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator-batch.newsforyou-recommendator-batch-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar --replicas 1 --network services --restart-delay 1h timonback/newsforyou-recommendator-batch:latest
+
+#Add spark task submitter - recommendation streamer
+docker service create --name spark-submitter-recommender-streamer -e MONGO_ADDRESS=mongo -e KAFKA_ADDRESS=kafka -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator-streamer.newsforyou-recommendator-streamer-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar,/opt/docker/lib/org.apache.kafka.kafka_2.11-0.8.2.2.jar,/opt/docker/lib/org.apache.kafka.kafka-clients-0.8.2.2.jar,/opt/docker/lib/org.apache.spark.spark-streaming-kafka-0-8_2.11-2.0.0-preview.jar,/opt/docker/lib/com.101tec.zkclient-0.3.jar --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator-streamer:latest
 
