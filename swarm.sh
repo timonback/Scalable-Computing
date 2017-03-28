a8ab02934aac30bf24324ce64978e67253a709f5
Timon Back <T.Back@student.rug.nl>
2017-03-28 22:37:43 +0200

    Swarm: Create topic at startup

--- swarm.sh
+++ swarm.sh
@@ -40,10 +40,17 @@ docker service create --name zookeeper -p 2181:2181 --replicas 1 --constraint 'n
 #Add Kafka
 # bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
 # bin/kafka-topics.sh --zookeeper zookeeper:2181 --create --replication-factor 1 --partitions 1 --topic helloworld
-docker service create --name kafka -p 9092:9092 -e KAFKA_PORT=9092 -e KAFKA_ADVERTISED_PORT=9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -e KAFKA_ZOOKEEPER_CONNECT=tasks.zookeeper:2181 -e "HOSTNAME_COMMAND=ip r | awk '{ ip[\$3] = \$NF } END { print ( ip[\"eth0\"] ) }'" --replicas 1 --network services wurstmeister/kafka
+docker service create --name kafka -p 9092:9092 -e KAFKA_PORT=9092 -e KAFKA_ADVERTISED_PORT=9092 -e KAFKA_CREATE_TOPICS=ratings:1:1 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -e KAFKA_ZOOKEEPER_CONNECT=tasks.zookeeper:2181 -e "HOSTNAME_COMMAND=ip r | awk '{ ip[\$3] = \$NF } END { print ( ip[\"eth0\"] ) }'" --replicas 1 --network services wurstmeister/kafka
 
 #Add visualization service
 docker service create --name visualization -p 9000:9000 -e MONGO_ADDRESS=mongo --replicas 1 --network services timonback/newsforyou-visualization:latest
+
+#Add importer service (run every 24 hours)
+docker service create --name importer -e MONGO_ADDRESS=mongo --replicas 1 --network services --restart-delay 24h timonback/newsforyou-importer:latest
+
+#Add rating generator
+docker service create --name streamer -e KAFKA_ADDRESS=kafka --replicas 1 --network services --restart-delay 1m timonback/newsforyou-streamer:latest
+
 
 #Add spark master
 docker service create --name spark-master --hostname spark-master -p 7077:7077 -p 8080:8080 --replicas 1 --network services singularities/spark start-spark master
@@ -55,11 +62,5 @@ docker service create --name spark-worker --hostname spark-worker --replicas 2 -
 #docker service create --name spark-task-submitter --replicas 1 --network services --restart-delay 1m singularities/spark spark-submit --class org.apache.spark.examples.SparkPi --master spark://spark-master:7077 /usr/local/spark-2.1.0/examples/jars/spark-examples_2.11-2.1.0.jar 10
 
 #Add spark task submitter (1min waiting inbetween runs) - recommendation
-docker service create --name spark-recommender-submitter -e MONGO_ADDRESS=mongo -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator.newsforyou-recommendator-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar -e USE_DUMMY_DATA=true --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator:latest
-
-#Add importer service (run every 24 hours)
-docker service create --name importer -e MONGO_ADDRESS=mongo --replicas 1 --network services --restart-delay 24h timonback/newsforyou-importer:latest
-
-#Add rating generator
-docker service create --name streamer -e KAFKA_ADDRESS=kafka --replicas 1 --network services --restart-delay 1m timonback/newsforyou-streamer:latest
+docker service create --name spark-recommender-submitter -e MONGO_ADDRESS=mongo -e KAFKA_ADDRESS=kafka -e SPARK_ADDRESS=spark-master -e SPARK_JAR=/opt/docker/lib/newsforyou-recommendator.newsforyou-recommendator-latest.jar,/opt/docker/lib/org.mongodb.spark.mongo-spark-connector_2.11-2.0.0.jar,/opt/docker/lib/org.mongodb.mongo-java-driver-3.2.2.jar -e USE_DUMMY_DATA=true --replicas 1 --network services --restart-delay 1m timonback/newsforyou-recommendator-streamer:latest
 
